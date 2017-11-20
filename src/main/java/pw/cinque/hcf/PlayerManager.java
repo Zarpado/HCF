4bb79f041465e6ea4d7d6cad7fdea641fed456d5
GitFyu <stimpypvp@gmail.com>
2017-11-20 22:57:26 +0100

    Fixed some methods that were returning an instance of the player implementation class instead of the interface

--- src/main/java/pw/cinque/hcf/PlayerManager.java
+++ src/main/java/pw/cinque/hcf/PlayerManager.java
@@ -13,29 +13,29 @@ public class PlayerManager {
     @Getter
     private static final PlayerManager instance = new PlayerManager();
 
-    private Map<UUID, FactionPlayerImpl> players = new MapMaker().weakValues().makeMap();
+    private Map<UUID, FactionPlayer> players = new MapMaker().weakValues().makeMap();
 
     private PlayerManager() {
     }
 
     /**
-     * Gets the {@link FactionPlayerImpl} for a certain {@link Player}
+     * Gets the {@link FactionPlayer} for a certain {@link Player}
      *
      * @param player The {@link Player}
-     * @return The {@link FactionPlayerImpl}
+     * @return The {@link FactionPlayer}
      */
-    public FactionPlayerImpl getFactionPlayer(Player player) {
+    public FactionPlayer getFactionPlayer(Player player) {
         return getFactionPlayer(player.getUniqueId(), player.getName());
     }
 
     /**
-     * Gets the {@link FactionPlayerImpl} for a a certain UUID and username
+     * Gets the {@link FactionPlayer} for a a certain UUID and username
      *
      * @param uniqueId The player's UUID
      * @param name     The player's username
-     * @return The {@link FactionPlayerImpl}
+     * @return The {@link FactionPlayer}
      */
-    public FactionPlayerImpl getFactionPlayer(UUID uniqueId, String name) {
+    public FactionPlayer getFactionPlayer(UUID uniqueId, String name) {
         return players.computeIfAbsent(uniqueId, key -> new FactionPlayerImpl(uniqueId, name));
     }
 
